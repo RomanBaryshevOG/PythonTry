450673e24515590b3bb7afb710cfb762bf86f2c8
Roman Baryshev <108750406+RomanBaryshevOG@users.noreply.github.com>
2022-10-16 13:47:44 +0500

    Added tsk3

---
+++ Tsk3.py
@@ -0,0 +1,23 @@
+# Написать программу получающую набор произведений чисел от 1 до N.
+
+# исходник
+# N = int(input('Введите число: '))
+
+# def get_factorial_list(n):
+#     fact = 1
+#     facts = []
+#     for i in range(1, n+1):
+#         fact *= i
+#         facts.append(fact)
+#     return facts
+
+# print(get_factorial_list(N))
+
+from itertools import accumulate
+import operator
+
+N = int(input('Введите число: '))
+
+def get_prods(N):
+    return list(accumulate([x for x in range(1, N+1)], operator.mul))
+print(get_prods(N))
